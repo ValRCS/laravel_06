bdb9c84dc9e8ce4cc08ecf531b29fa4f2fae0227
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-18 12:58:33 +0300

    Monolog example

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+echo "Hello there";
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+// create a log channel
+$log = new Logger('myLogger');
+$log->pushHandler(new StreamHandler('../log/my.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
