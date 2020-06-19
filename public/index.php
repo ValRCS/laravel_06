596cb665a26018da8f794e1b577d10b2fc9139bf
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-19 09:37:54 +0300

    syslog examples with Monolog

--- public/index.php
+++ public/index.php
@@ -1,14 +1,22 @@
 <?php
 require '../vendor/autoload.php'; //to find automatically vendor libraries
-echo "Hello there";
+echo "Hello there I am logging";
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
 // create a log channel
+// $log = new Monolog\Logger('myLogger');
 $log = new Logger('myLogger');
-$log->pushHandler(new StreamHandler('../log/my.log', Logger::WARNING));
+// $log->pushHandler(new Monolog\Handler\StreamHandler('../log/my.log', Monolog\Logger::DEBUG));
+//with use we can write the short version
+$log->pushHandler(new StreamHandler('../log/mysecond.log', Logger::DEBUG));
 
 // add records to the log
+// var_dump($_SERVER);
+$log->info($_SERVER['HTTP_USER_AGENT']);
 $log->warning('Foo');
+$log->debug("Lowest of the low");
 $log->error('Bar');
+$log->info('Morning');
+$log->debug("Lowest of the low");
